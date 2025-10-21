d5fbe135f825094533c412d8d052a5c12009a527
Michael Barasa <mikebarasa@outlook.com>
2025-10-21 17:04:59 +0300

    chore: Add junit reporter

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    reporter: 'cypress-multi-reporters',
+    reporterOptions: {
+      configFile: 'reporter-config.json',
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
